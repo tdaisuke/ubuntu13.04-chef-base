2da2094908d7bcf9b9adea360221e9d9fc29af43
vagrant <github@samedai.com>
2014-01-19 08:24:31 +0000

    add git

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,8 +9,8 @@ sudo apt-get install -y libcurl3-gnutls
 sudo apt-get install -y libcurl4-openssl-dev 
 sudo apt-get install -y c++
 sudo apt-get install -y g++
-sudo apt-get -y upgrade
-sudo gem install nokogiri aws-sdk mechanize mws-connect
-sudo gem install sinatra
-sudo gem install mail
+sudo apt-get install -y git
+gem install nokogiri aws-sdk mechanize mws-connect
+gem install sinatra
+gem install mail
 
